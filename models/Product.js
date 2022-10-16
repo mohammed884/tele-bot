f770beb6a78f18db80c153f34b1c484c2c46cf84
Mohammed Abdulaziz <80047190+mohammed884@users.noreply.github.com>
2022-10-16 22:04:33 +0300

    Add files via upload

---
+++ models/Product.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    messageId: {
+        type: Number,
+        required: true,
+    },
+});
+const Product = new mongoose.model("products", Schema);
+export default Product
